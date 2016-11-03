52efff895de9d4597d38b69bd054a83cce9023c2
Rafael Vinicius Barros Ferreira <rafael_viniciusbarros@hotmail.com>
2016-11-03 17:07:04 -0200

    Joguei no repositorio a documentação da monografia, e fiz as classes no lado cliente para medicamentos precificados

---
+++ site/js/laboratorio/laboratorio.serv.js
@@ -0,0 +1,40 @@
+/**
+ *  laboratorio.serv.js
+ *  
+ *  @author	Rafael Vinicius Barros Ferreira
+ */
+ (function(app, $)
+ {
+	'use strict';
+
+	function Laboratorio(
+		id,
+		nome
+	) 
+	{
+		this.id = id || 0; 
+		this.nome = nome || ''; 
+	};
+	
+	function ServicoLaboratorio()
+	{ // Model
+		var _this = this;
+		
+		// Cria um objeto de Laboratorio
+		this.criar = function criar(
+			id,
+			nome
+		) 
+		{
+ 			return {
+				id : id || 0,
+				nome : nome ||
+			};
+		};
+	}; // ServicoLaboratorio
+	
+	// Registrando
+	app.Laboratorio = Laboratorio;
+	app.ServicoLaboratorio = ServicoLaboratorio;
+
+})(app, $);
---
+++ site/js/pincipio ativo/principioAtivo.serv.js
@@ -0,0 +1,40 @@
+/**
+ *  principioAtivo.serv.js
+ *  
+ *  @author	Rafael Vinicius Barros Ferreira
+ */
+ (function(app, $)
+ {
+	'use strict';
+
+	function PrincipioAtivo(
+		id,
+		nome
+	) 
+	{
+		this.id = id || 0; 
+		this.nome = nome || ''; 
+	};
+	
+	function ServicoPrincipioAtivo()
+	{ // Model
+		var _this = this;
+		
+		// Cria um objeto de PrincipioAtivo
+		this.criar = function criar(
+			id,
+			nome
+		) 
+		{
+ 			return {
+				id : id || 0,
+				nome : nome ||
+			};
+		};
+	}; // ServicoPrincipioAtivo
+	
+	// Registrando
+	app.PrincipioAtivo = PrincipioAtivo;
+	app.ServicoPrincipioAtivo = ServicoPrincipioAtivo;
+
+})(app, $);
