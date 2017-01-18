16322df8b6a6dd5d1590a8c446c5815b41c5863a
Rafael Vinicius Barros Ferreira <rafael_viniciusbarros@hotmail.com>
2017-01-18 00:57:53 -0200

    Criei classes enumeradas do sistema

---
+++ site/api/modules/administracao/Administracao.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *	Administracao
+ *
+ *  @author Rafael Vinicius Barros Ferreira
+ *  @version	1.0
+ */
+abstract class Administracao {
+	
+	const ADMINISTRACAO_ORAL = 'Oral';
+	const ADMINISTRACAO_SUBLINGUAL = 'Sublingual';
+	const ADMINISTRACAO_RETAL = 'Retal';
+	const ADMINISTRACAO_INTRA_VENOSA = 'Intra-Venosa';
+	const ADMINISTRACAO_INTRA_MUSCULAR = 'Intra-Muscular';
+	const ADMINISTRACAO_SUBCUTÂNEA = 'Subcutânea';
+	const ADMINISTRACAO_INTRADÉRMICA = 'Intradérmica';
+	const ADMINISTRACAO_INTRA_ARTERIAL =  'Intra-arterial';
+	const ADMINISTRACAO_INTRACARDÍACA =  'Intracardíaca';
+	const ADMINISTRACAO_INTRATECAL =  'Intratecal';
+	const ADMINISTRACAO_PERIDURAL =  'Peridural';
+	const ADMINISTRACAO_INTRA_ARTICULAR =  'Intra-articular';
+	const ADMINISTRACAO_CUTÂNEA =  'Cutânea';
+	const ADMINISTRACAO_RESPIRATÓRIA =  'Respiratória';
+	const ADMINISTRACAO_CONJUNTIVAL =  'Conjuntival';
+	const ADMINISTRACAO_GENITURINÁRIA =  'Geniturinária';
+	const ADMINISTRACAO_INTRACANAL =  'Intracanal';
+}
+
+?>
---
+++ site/api/modules/periodicidade/UnidadeMassaOuPeso.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *	UnidadeMassaOuPeso
+ *
+ *  @authoRafael Vinicius Barros Ferreira
+ *  @version	1.0
+ */
+abstract class UnidadeMassaOuPeso{
+	
+	const GRAMA = 'Grama';
+	const Micrograma = 'Micrograma';
+	const MICROGRAMA = 'Micrograma';
+	const MILIGRAMA = 'Miligrama';
+}
+
+?>
