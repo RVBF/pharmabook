737d5f231599b6d2e136fcb234bdae63c6050042
Rafae Vinicius Barros <rafaelviniciusbarros5@gmail.com>
2017-01-18 00:57:53 -0200

    Criei classes enumeradas do sistema

---
+++ site/api/modules/periodicidade/UnidadeVolume.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *	UnidadeVolume
+ *
+ *  @authoRafael Vinicius Barros Ferreira
+ *  @version	1.0
+ */
+abstract class UnidadeVolume{
+	
+	const LITRO = 'Litro';
+	const MILILITRO = 'Mililitro';
+}
+
+?>
