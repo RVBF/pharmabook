579bbe5240e3182d7fcc9a0d750eb62425b94b3e
Rafae Vinicius Barros <rafaelviniciusbarros5@gmail.com>
2017-01-23 22:24:20 -0200

    corrigi erros

--- site/api/modules/tipo medicamento pessoal/MedicamentoForma.php
+++ site/api/modules/tipo medicamento pessoal/MedicamentoForma.php
@@ -20,12 +20,7 @@ abstract class MedicamentoForma extends Enum {
 	const PO = 'Pó';
 
 	//Formas de medicamentos Líquida
-	const ORAIS = 'Orais';
-	const TOPICAS = 'Tópicas';
-	const PARENTERAIS = 'Parenterais';
-	const SOLUCOES = 'Solucões';
-	const EMULSOES = 'Emulsões';
-	const SUSPENSOES = 'Suspensões';
+	const LIQUIDO = 'Líquido';
 }
 
 ?>
