fb071013cd1cf9a6773c2587936b08222e204c1b
Rafael Vinicius Barros Ferreira <rafaelviniciusbarros5@gmail.com>
2018-01-07 17:46:09 -0200

    Evolui o script de iimportação das formas de medicamento,

--- site/api/app/Classes/modules/importacao/ColecaoImportacaoVocabuluarioMedicamentosEmBDR.php
+++ site/api/app/Classes/modules/importacao/ColecaoImportacaoVocabuluarioMedicamentosEmBDR.php
@@ -14,7 +14,7 @@ class ColecaoImportacaoVocabuluarioMedicamentosEmBDR implements ColecaoImportaca
 
 	private $pdoW;
 
-	private $registros;
+	private $formasFarmaceuticas;
 
 	function __construct(PDOWrapper $pdoW)
 	{
@@ -25,54 +25,92 @@ class ColecaoImportacaoVocabuluarioMedicamentosEmBDR implements ColecaoImportaca
 	{
 		try
 		{
+			$formasFarmaceuticas = [];
+			$contadorRegistrosTipos = 0;
+			$posisaoInicial = null;
+			$indiceAtual = 0;
 
-			$registros = [];
-
-			for($contadorArquivos = 14; $contadorArquivos <= 58; $contadorArquivos++)
+			for($contadorArquivos = 14; $contadorArquivos <= 34; $contadorArquivos++)
 			{
 				$pasta = realpath(dirname('..\\')) . '\\..\\..\\..\\..\\..\\..\\bd\\vocabulario\\';;
 				$dom = new DOMDocument();
 				$dom->loadHTMLFile($pasta . $contadorArquivos . ".html");
 				$divs = $dom->getElementsByTagName('div');
+				$incompleta = false;
 
 				foreach ($divs as  $key => $div)
 				{
-					$valorAtual = utf8_encode($div->nodeValue);
+					$valorAtual = utf8_decode($div->nodeValue);
+
+					if($posisaoInicial != null and $posisaoInicial > 0)
+					{
+						if(strcmp($valorAtual,'Abreviação:') == 0)
+						{
+							if(isset($formasFarmaceuticas[$posisaoInicial])) $formasFarmaceuticas[$posisaoInicial]['abreviacao'] = (isset($divs->item($contador + 1)->nodeValue) ) ? utf8_decode($divs->item($key + 1)->nodeValue) : '';
+							for($posicaoDiv = $key; $key >= 0; $key--)
+							{
+								if(isset($formasFarmaceuticas[$posisaoInicial])) $formasFarmaceuticas[$posisaoInicial]['descricao'] .= (isset($divs->item($contador)->nodeValue)) ? utf8_decode($divs->item($contador)->nodeValue) : '';
+							}
+						}
+
+						$posisaoInicial = null;
+					}
 
 					if(strcmp($valorAtual,'Conceito:') == 0)
 					{
-						$registros[]['nome'] = utf8_encode($divs->item($key -1)->nodeValue);
+						$contadorRegistrosTipos ++;
+						$formasFarmaceuticas[$indiceAtual]['nome'] = utf8_decode($divs->item($key -1)->nodeValue);
 						$contador = $key + 1;
-						$indiceAtual = count($registros) - 1;
-
-						$registros[$indiceAtual]['descricao'] = '';
+						$formasFarmaceuticas[$indiceAtual]['descricao'] = '';
 
 						while($contador > 0)
 						{
-							$divAtual =	isset($divs->item($contador)->nodeValue) ? utf8_encode($divs->item($contador)->nodeValue) : '';
+							$divAtual =	isset($divs->item($contador)->nodeValue) ? utf8_decode($divs->item($contador)->nodeValue) : '';
 
-							if(strcmp($divAtual,'Conceito:') == 0)
+							if(strcmp($divAtual, 'Abreviação') == 0)
 							{
-								if(isset($registros[$indiceAtual])) $registros[$indiceAtual]['abreviacao'] = (isset($divs->item($contador + 1)->nodeValue) ) ? utf8_encode($divs->item($key + 1)->nodeValue) : '';
+								if(isset($formasFarmaceuticas[$indiceAtual])) $formasFarmaceuticas[$indiceAtual]['abreviacao'] = (isset($divs->item($contador + 1)->nodeValue) ) ? utf8_decode($divs->item($key + 1)->nodeValue) : '';
 								break;
 							}
 							else
 							{
-								if(isset($registros[$indiceAtual])) $registros[$indiceAtual]['descricao'] .= (isset($divs->item($contador)->nodeValue)) ? utf8_encode($divs->item($contador)->nodeValue) : '';
+								if(isset($formasFarmaceuticas[$indiceAtual])) $formasFarmaceuticas[$indiceAtual]['descricao'] .= (isset($divs->item($contador)->nodeValue)) ? utf8_decode($divs->item($contador)->nodeValue) : '';
 							}
 
 							if($contador + 1 > $divs->length)
 							{
+								$incompleta = true;
+								$posisaoInicial = $indiceAtual;
 								break;
 							}
 
 							$contador ++;
 						}
-					}
-				}
-			}
 
-			$this->registros = $registros;
+						if($contadorArquivos <= 25)
+						{
+							$formasFarmaceuticas[$indiceAtual]['estado'] = FormaFarmaceutica::SOLIDO;
+						}
+						else if($contadorArquivos <= 32)
+						{
+							$formasFarmaceuticas[$indiceAtual]['estado'] = FormaFarmaceutica::LIQUIDO;
+						}
+						else if($contadorArquivos <= 34)
+						{
+							if($contadorRegistrosTipos == 0) $formasFarmaceuticas[$indiceAtual]['estado'] = FormaFarmaceutica::LIQUIDO;
+							else if($contadorRegistrosTipos <= 5) $formasFarmaceuticas[$indiceAtual]['estado'] = FormaFarmaceutica::SEMI_SOLIDO;
+							else if($contadorRegistrosTipos == 6) $formasFarmaceuticas[$indiceAtual]['estado'] = FormaFarmaceutica::GASOSO;
+						}
+					}
+
+					$indiceAtual++;
+				}
+
+				if($contadorArquivos == 26 or $contadorArquivos == 33) $contadorRegistrosTipos = 0;
+			}
+			Debuger::printr($formasFarmaceuticas);
+
+			$this->formasFarmaceuticas = $formasFarmaceuticas;
 		}
 		catch (\Exception $e)
 		{
--- site/api/app/Classes/modules/medicamento pessoal/FormaFarmaceutica.php
+++ site/api/app/Classes/modules/medicamento pessoal/FormaFarmaceutica.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- *	FormasFarmaceuticas
+ *	FormaFarmaceutica
  *
  *  @author Rafael Vinicius Barros Ferreira
  *  @version	1.0
  */
-abstract class FormasFarmaceuticas extends Enum {
+abstract class FormaFarmaceutica extends Enum {
 
 	// Formas de medicamento semi-sólida
 	const SOLIDO = 'Sólido';
 	const SEMI_SOLIDO = 'Semi-Sólido';
 	const LIQUIDO  = 'Líquido';
-	const GASOSOS = 'Gasosos';
+	const GASOSO = 'Gasosos';
 }
 
 ?>
